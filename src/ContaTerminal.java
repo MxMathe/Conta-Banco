344cf7aea579e5d4ada21668773c82a614a80f05
Matheus Dias Lopes <72584724+MxMathe@users.noreply.github.com>
2025-05-11 20:10:40 -0300

    Update ContaTerminal.java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        // Usando try-with-resources para garantir o fechamento do Scanner
+        
         try (Scanner scanner = new Scanner(System.in)) {
-            // Solicitando e armazenando os dados
+            
             System.out.println("Por favor, digite o número da Conta:");
             int numero = scanner.nextInt();
 
-            // Consumir a quebra de linha pendente após nextInt
+            
             scanner.nextLine();
 
             System.out.println("Por favor, digite o número da Agência:");
@@ -20,7 +20,7 @@ public class ContaTerminal {
             System.out.println("Por favor, digite o saldo da Conta:");
             double saldo = scanner.nextDouble();
 
-            // Exibindo a mensagem final com concatenação
+            
             System.out.println("\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " 
                 + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
         } catch (Exception e) {
